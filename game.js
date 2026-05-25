c48acee10476d36e838403abe9a2b3797a6ccbe6
Dominik Vékony <dominikvekony11@gmail.com>
2026-05-25 17:14:37 +0200

    index, style, js hozzáadva

---
+++ game.js
@@ -0,0 +1,22 @@
+const player = {
+
+  skill: 0,
+
+  maxHealth: 20,
+
+  health: 20,
+
+  luck: 0,
+
+  gold: 12,
+
+  food: 10,
+
+  inventory: [
+    "Kard",
+    "Pajzs",
+    "Lámpás",
+    "Kötél"
+  ]
+
+};
